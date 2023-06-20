cbc35bb9ec31b2fde6d111bd6d7176cdf6c2f925
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-20 16:17:18 -0600

    primera version

---
+++ Funciones pt.1.py
@@ -0,0 +1,14 @@
+# Funciones sin retorno de valor
+
+def saludar(nombre):
+    print(f"Hola {nombre}")
+
+saludar("Caro")
+saludar("Elias")
+
+def multiplicar(num):
+    for i in range(1,11):
+        print(f"{num}*{i} = {num*i}")
+
+multiplicar(5)
+multiplicar(3)
